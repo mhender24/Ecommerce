9ac30e8ad76901e40f27dc68e6f79757c17912a7
mhender24 <mhenderson@archlt.com>
2017-02-03 13:30:27 -0500

    Added functionality to send emails in /Home/Contact, still needs appropriate web.config setup

--- Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ecommerce.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace Ecommerce.Controllers
 {
@@ -16,6 +20,32 @@ namespace Ecommerce.Controllers
             customerDb = new CustomerRepository(new Data());
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(Email model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("mhender24@gotimetechnologies.com")); //replace with valid value
+                message.To.Add(new MailAddress("mbolin@gotimetechnologies.com")); //replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
+            return View();
+        }
 
         public ActionResult Index()
         {
--- Ecommerce/Models/IdentityModels.cs
+++ Ecommerce/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Ecommerce.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("Data", throwIfV1Schema: false)
         {
         }
 
