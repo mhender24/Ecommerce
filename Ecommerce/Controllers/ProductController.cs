938cb6146a8cd3ef79650e81b10fe0a2e95be627
mhender24 <mhenderson@archlt.com>
2017-02-10 11:04:13 -0500

    Attach categories to products in create page

--- Ecommerce/Controllers/ProductController.cs
+++ Ecommerce/Controllers/ProductController.cs
@@ -88,16 +88,22 @@ namespace Ecommerce.Controllers
         public ActionResult Create()
         {
             ViewBag.SupplierId = new SelectList(_db.SupplierRepository.Get(), "Id", "Name");
+            ViewBag.Categories = new MultiSelectList(_db.CategoryRepository.Get(), "Id", "Name");
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,SupplierId,Name,Price,Description,ProductImage")] Product product)
+        public ActionResult Create([Bind(Include = "Id,SupplierId,Name,Price,Description,ProductImage,CategoryId")] Product product, int[] categoryId)
         {
             if (ModelState.IsValid)
             {
                 _db.ProductRepository.Insert(product);
+                _db.Save();
+                foreach (var catId in categoryId)
+                {
+                    _db.ProductCategoryRepository.Insert(new ProductCategory { ProductId = product.Id, CategoryId = catId });
+                }
                 _db.Save();
                 return RedirectToAction("Index");
             }
